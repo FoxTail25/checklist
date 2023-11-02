38542ba3f4bfd8ede6d9296ddfc0afb6ac4d5af4
Fox Tail <foxtail25@gmail.com>
2023-11-02 14:40:20 +0300

    CheckListApp

---
+++ src/components/CheckList/checklist/CheckList.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+
+let bisnesArr = [
+    { bisnes: 'дело1', comlite: 'false' },
+    { bisnes: 'дело2', comlite: 'false' },
+    { bisnes: 'дело3', comlite: 'false' },
+  ]
+
+export const CheckList = () => {
+
+    const [bisnes, setBisnes] = useState(bisnesArr)
+
+
+  return (
+    <div>
+
+    </div>
+  )
+}
