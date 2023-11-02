dab994f3b3f98ed63d4c1714128b709242e0eb27
Fox Tail <foxtail25@gmail.com>
2023-11-02 17:55:35 +0300

    Логика готова. Начало стилицации

--- src/components/CheckList/biznes/Biznes.jsx
+++ src/components/CheckList/biznes/Biznes.jsx
@@ -1,23 +1,44 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
+import { CheckListContext } from '../contextCheckList'
+import styles from './biznes.module.css'
+// import 
 
-export const Biznes = ({ record, changeRecord}) => {
+export const Biznes = ({ record }) => {
+
+  const [isEdit, setIsEdit] = useState(false)
 
   const { id, bisnes, complite } = record
 
- 
-  console.log(complite);
+  const {status, text, del} = useContext(CheckListContext)
+
+
+  // console.log(status);
 
   return (
-    <li>
-      <div style={{display: "flex", justifyContent: "space-between"}}>
-        {bisnes}
-        <span onClick={() => changeRecord(id)}>
+    <li className={complite ? styles.complite : styles.uncomplite}>
+      <div>
+
+        <span style={{ padding: "0 5px" }} onClick={() => status(id)}>
           {
             complite
               ? "выполнено"
               : "не выполнено"
           }
         </span>
+
+        {
+          isEdit
+            ?
+            <>
+              <input  style={{ padding: "0 5px" }} value={bisnes} onChange={event => text(id, event.target.value)} />
+              <button onClick={() => setIsEdit(!isEdit)}>save</button>
+            </>
+
+            : <span onClick={() => setIsEdit(!isEdit)}>
+              {bisnes}
+            </span>
+        }
+        <button onClick={()=>del(id)}>удалить запись</button>
       </div>
     </li>
   )
--- src/components/CheckList/checklist/CheckList.jsx
+++ src/components/CheckList/checklist/CheckList.jsx
@@ -2,35 +2,65 @@ import React, { useState } from 'react'
 import { Biznes } from '../biznes/Biznes'
 import { nanoid } from 'nanoid'
 import { CheckListContext } from '../contextCheckList'
+import styles from './checklist.module.css'
 
 
 let bisnesArr = [
-  { id: nanoid(), bisnes: 'дело1', complite: false },
-  { id: nanoid(), bisnes: 'дело2', complite: false },
-  { id: nanoid(), bisnes: 'дело3', complite: false },
+  { id: nanoid(), bisnes: 'Дело №1', complite: false },
+  { id: nanoid(), bisnes: 'Дело №2', complite: false },
+  { id: nanoid(), bisnes: 'Дело №3', complite: false },
 ]
 
 export const CheckList = () => {
 
   const [bisnes, setBisnes] = useState(bisnesArr)
 
-  function changeRecord(id) {
+  function changeRecordStatus(id) {
     setBisnes([...bisnes.map(el => {
-      if(el.id === id) {
+      if (el.id === id) {
         el.complite = !el.complite
       }
       return el
     })])
   }
 
-  let result = bisnes.map((record) => <Biznes key={record.id} record={record} changeRecord={changeRecord}/>)
+  function changeRecordText(id, text) {
+    setBisnes([...bisnes.map(el => {
+      if (el.id === id) {
+        el.bisnes = text
+      }
+      return el
+    })])
+  }
+
+  function addRecord() {
+    let bisnesNumber = bisnes.length + 1;
+    setBisnes([...bisnes, { id: nanoid(), bisnes: `Дело №${bisnesNumber}`, complite: false }])
+  }
+
+  function dellRecord(id) {
+    setBisnes([...bisnes.filter(record => record.id !== id)])
+  }
+
+  const changeRecord = {
+    status: changeRecordStatus,
+    text: changeRecordText,
+    del: dellRecord
+  }
+
+
+  let result = bisnes.map((record) => <Biznes key={record.id} record={record} />)
 
 
   return (
-    <ol>
-    <CheckListContext.Provider value={bisnes}>
-    {result}
-    </CheckListContext.Provider>
-    </ol>
+    <div className={styles.checklist__bg}>
+      <h1>Список дел</h1>
+      <ol>
+        <CheckListContext.Provider value={changeRecord}>
+          {result}
+        </CheckListContext.Provider>
+      </ol>
+      <button onClick={addRecord}>добавить дело</button>
+    </div>
   )
 }
