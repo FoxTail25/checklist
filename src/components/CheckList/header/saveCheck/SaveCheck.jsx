bdd9568cb4c596add614d8f60a8802ff0357117a
Grigoriy <foxtail25@gmail.com>
2023-11-05 11:59:14 +0300

    SaveBtn добавил классы убрал закомментил переклю

--- src/components/CheckList/header/saveCheck/SaveCheck.jsx
+++ src/components/CheckList/header/saveCheck/SaveCheck.jsx
@@ -2,45 +2,35 @@ import React, { useState } from 'react'
 import s from './sassCSS/saveCheck.module.css'
 
 export const SaveCheck = () => {
-    
-    // let savePlace;
 
-    // let local = true;
-    // let server = false;
-    
+    let saveData;
+
     let dataBtn = JSON.parse(localStorage.getItem('saveBtn'))
+    if (dataBtn) {
+        saveData = dataBtn;
+    } else {
+        saveData = false
+    }
 
-    // if(dataBtn) {
-    //     setSave(dataBtn)
-    // }
-    
-    const [save, setSave] = useState(dataBtn)
-     
-
+    const [save, setSave] = useState(saveData)
 
     function changeSaveBtn() {
-        dataBtn = !save
         setSave(!save)
-        localStorage.setItem('saveBtn', JSON.stringify(save))
+        localStorage.setItem('saveBtn', JSON.stringify(!save))
     }
-    
 
-    // if (save) {
-    //     savePlace = true
-     
-    // } else {
-    //     savePlace = false
-    // }
 
-    // console.log('render')
+    // const [local, setLocal] = useState(true)
+    // const [server, setServer] = useState(false)
 
-    // function changeButton() {
-    //     local = !local
-    //     server = !server
-    //     // setSave(save)
-    //     console.log('da')
-    // }
+    function changeSavePlace() {
+        // setLocal(!local)
+        // setServer(!server)
 
+        // console.log(local, server)
+    }
+
+console.log('save', save)
 
     return (
         <div className={s.container}>
@@ -48,10 +38,12 @@ export const SaveCheck = () => {
                 сохранять изменения<input type='checkbox' checked={save} onChange={changeSaveBtn} />
             </p>
 
-            {/* <div className={s.buttons}>
-                <button disabled = {!local} onClick={changeButton}>локально <br/>(в браузере)</button>
-                <button disabled = {!server} onClick={changeButton}>глобально  <br/>(на сервере)</button>
-            </div> */}
+            <div className={s.buttons}>
+                {/* <button disabled = {!(save && local)} onClick={changeSavePlace}>локально <br/>(в браузере)</button>
+                <button disabled = {!(save && server)} onClick={changeSavePlace}>глобально  <br/>(на сервере)</button> */}
+                <button disabled={!save} className={save ? s.active: ''} onClick={changeSavePlace}>локально <br/>(в браузере)</button>
+                <button disabled onClick={changeSavePlace}>глобально  <br/>(на сервере)</button>
+            </div>
         </div>
     )
 }
